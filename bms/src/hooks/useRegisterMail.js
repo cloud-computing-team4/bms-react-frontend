f774e3e2a4d8f645e602c02901b5deb9d08f028c
JaeAnHan <jaean1999@naver.com>
2024-06-06 18:51:35 +0900

    feat: 이메일 변경시 재왁인을 confirm으로 변경

--- bms/src/hooks/useRegisterMail.js
+++ bms/src/hooks/useRegisterMail.js
@@ -33,8 +33,8 @@ export const useRegisterMail = () => {
         toast.success(`${r.data.data.id}으로 등록 완료`);
         toast("메일 전송 요청을 보냈습니다.");
         const cleanup = () => {
-          setMail("");
-          setTitle("");
+          // setMail("");
+          // setTitle("");
         };
         sendMail(r.data.data.id, cleanup);
       })
@@ -78,74 +78,3 @@ export const useRegisterMail = () => {
 
   return { setTitle, setMail, setIsRegisterClicked, setIsResAndSendClicked };
 };
-
-// import { useEffect, useState } from "react";
-// import toast from "react-hot-toast";
-// import axios from "axios";
-// import { useSendMail } from "./useSendMail";
-//
-// export const useRegisterMail = () => {
-//   const [title, setTitle] = useState("");
-//   const [mail, setMail] = useState("");
-//   const [isRegisterClicked, setIsRegisterClicked] = useState(false);
-//   const [isResAndSendClicked, setIsResAndSendClicked] = useState(false);
-//   const { sendMail } = useSendMail();
-//
-//   useEffect(() => {
-//     if (!isResAndSendClicked) {
-//       return;
-//     }
-//     if (title.trim().length === 0) {
-//       toast.error("제목을 확인해주세요.");
-//       return;
-//     }
-//     if (mail.trim().length === 0) {
-//       toast.error("메일 본문을 확인해주세요.");
-//       return;
-//     }
-//     axios
-//       .post("/mail", {
-//         title: title,
-//         content: mail,
-//       })
-//       .then((r) => {
-//         toast.success(`${r.data.data.id}으로 등록 완료`);
-//         toast("메일 전송 요청을 보냈습니다.");
-//         const cleanup = () => {
-//           setMail("");
-//           setTitle("");
-//         };
-//         sendMail(r.data.id, cleanup);
-//       })
-//       .catch((e) => toast.error(e.response.message ?? "메일 등록 요청 실패"));
-//     setIsResAndSendClicked(false);
-//   }, [isResAndSendClicked, mail, title]);
-//
-//   useEffect(() => {
-//     if (!isRegisterClicked) {
-//       return;
-//     }
-//     if (title.trim().length === 0) {
-//       toast.error("제목을 확인해주세요.");
-//       return;
-//     }
-//     if (mail.trim().length === 0) {
-//       toast.error("메일 본문을 확인해주세요.");
-//       return;
-//     }
-//     axios
-//       .post("/mail", {
-//         title: title,
-//         content: mail,
-//       })
-//       .then((r) => {
-//         toast.success(r.data);
-//         // setMail("");
-//         // setTitle("");
-//       })
-//       .catch((e) => toast.error(e.response.message ?? "메일 등록 요청 실패"));
-//     setIsRegisterClicked(false);
-//   }, [isRegisterClicked, mail, title]);
-//
-//   return { setTitle, setMail, setIsRegisterClicked, setIsResAndSendClicked };
-// };
--- bms/src/hooks/useSendMail.js
+++ bms/src/hooks/useSendMail.js
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import axios from "../axiosConfig"; // axiosConfig에서 설정한 axios 인스턴스를 사용합니다.
+import axios from "../axiosConfig";
 import toast from "react-hot-toast";
 
 export const useSendMail = () => {
@@ -19,25 +19,3 @@ export const useSendMail = () => {
 
   return { sendMail };
 };
-
-// import { useCallback } from "react";
-// import axios from "axios";
-// import toast from "react-hot-toast";
-//
-// export const useSendMail = () => {
-//   const sendMail = useCallback((id, cleanup = undefined) => {
-//     axios
-//       .get(`bulk-mail/${id}`)
-//       .then((r) => {
-//         toast.success(r.data);
-//         if (cleanup) {
-//           cleanup();
-//         }
-//       })
-//       .catch((r) => {
-//         toast.error(r.response.data ?? "메일 전송 요청 실패");
-//       });
-//   }, []);
-//
-//   return { sendMail };
-// };
