2d46091519822acdd63c6f3f5525c5baecac403e
이수빈 <02ggang9@gmail.com>
2024-06-06 19:53:08 +0900

    fix: 8080 포트로 수정

--- bms/src/axiosConfig.js
+++ bms/src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "https://54.180.233.127:443",
+  baseURL: "http://54.180.233.127:8080",
   withCredentials: true,
 });
 export default instance;
