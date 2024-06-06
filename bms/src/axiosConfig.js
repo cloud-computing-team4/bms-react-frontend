bbb9fa52d4a2b179c7dd93e74b5d152feffcffff
JaeAnHan <jaean1999@naver.com>
2024-06-06 20:28:05 +0900

    feat: https 포트 변경

--- bms/src/axiosConfig.js
+++ bms/src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://54.180.233.127:443",
+  baseURL: "https://54.180.233.127:8080",
   withCredentials: true,
 });
 export default instance;
